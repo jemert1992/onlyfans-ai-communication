78cc18c346f60393b7d733a0fb0bb75e8b7d9c10
jemert1992 <justin@tapp.co>
2025-04-14 20:25:25 -0400

    Create wgsi.py

---
+++ wgsi.py
@@ -0,0 +1,11 @@
+import sys
+import os
+
+# Add the backend directory to the path
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'code_package/backend'))
+
+# Import the app
+from app import app
+
+if __name__ == "__main__":
+    app.run()
